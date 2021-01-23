ece68054fa7b1d40f0417c70e410276fa609e694
Aliaksandr Artsikhovich <art-orient@tut.by>
2021-01-23 21:17:47 +0300

    add maven-multi-module task

---
+++ maven_multi_module/Suffuxing_App/src/main/java/epam/art/suffixing_app/MyFile.java
@@ -0,0 +1,40 @@
+package epam.art.suffixing_app;
+
+import java.util.List;
+
+public class MyFile {
+    private List<String> file;
+    private String suffix;
+
+    public MyFile(List<String> file, String suffix) {
+        this.file = file;
+        this.suffix = suffix;
+    }
+
+    public MyFile() {
+    }
+
+    public List<String> getFile() {
+        return file;
+    }
+
+    public void setFile(List<String> file) {
+        this.file = file;
+    }
+
+    public String getSuffix() {
+        return suffix;
+    }
+
+    public void setSuffix(String suffix) {
+        this.suffix = suffix;
+    }
+
+    @Override
+    public String toString() {
+        return "MyFile{" +
+                "file=" + file +
+                ", suffix='" + suffix + '\'' +
+                '}';
+    }
+}
